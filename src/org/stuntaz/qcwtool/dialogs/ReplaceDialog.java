3a7505a141a7e7aaa70977e8c60ef3cff8f1021e
Jon Nall <jon.nall@gmail.com>
2009-04-29 09:58:23 -0700

    In replacement, allow 1 symbol to map to multiple symbols.     This is useful for things like cloning lines from those studies where     the range isn't price-based. Requested by Matthew Huff

--- src/org/stuntaz/qcwtool/dialogs/ReplaceDialog.java
+++ src/org/stuntaz/qcwtool/dialogs/ReplaceDialog.java
@@ -24,8 +24,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.layout.TableColumnLayout;
--- src/org/stuntaz/qcwtool/tools/ReplaceTool.java
+++ src/org/stuntaz/qcwtool/tools/ReplaceTool.java
@@ -27,6 +27,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
