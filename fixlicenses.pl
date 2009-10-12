f0327a74b7eecf906ddef776e2bd5539b584dd47
Jon Nall <jon.nall@gmail.com>
2009-10-12 15:32:53 -0700

    Updated to OSL 3.0

--- fixlicenses.pl
+++ fixlicenses.pl
@@ -69,16 +69,16 @@ sub addNewHeader()
  * libqcw - A library for parsing, manipulating, and writing QCharts (TM) 
  * workspace files.
  *
- * Copyright (C) 2008 Jon Nall
+ * Copyright (C) 2008-2009 Jon Nall
  * QCharts is a registered service mark of eSignal, Inc.
  * RagingBull is a registered service mark of eSignal, Inc.
  *
- * Licensed under the Open Software License version 2.0 (the "License"); you
+ * Licensed under the Open Software License version 3.0 (the "License"); you
  * may not use this file except in compliance with the License. You should
  * have received a copy of the License along with this software; if not, you
  * may obtain a copy of the License at
  *
- * http://opensource.org/licenses/osl-2.0.php
+ * http://opensource.org/licenses/osl-3.0.php
  *
  * This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the
